0ebfa98e87f4c4065607b1458014e000958e387c
gordonthree <gordonthree@gmail.com>
2022-07-29 14:32:22 -0400

    add some code

---
+++ src/packmaster.cpp
@@ -0,0 +1,509 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <MCP342x.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_MCP4725.h>
+#include <Adafruit_ADS1X15.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP32DMASPIMaster.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+
+ESP32DMASPI::Master master;
+
+const char* ssid = "Tell my WiFi I love her";
+const char* password = "2317239216";
+
+int16_t adc0, adc1, adc2, adc3;
+float volts0, volts1, volts2, volts3;
+char buff[50];
+long ch1 = 0;
+long ch2 = 0;
+float ich1, ich2;
+
+long acsOffset = 1679;
+long acsmvA = 90;
+float ampsch1, ampsch2;
+
+// 0x68 is the default address for all MCP342x devices
+uint8_t address = 0x68;
+MCP342x adc = MCP342x(address);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+/* Note: If flash space is tight a quarter sine wave is enough
+   to generate full sine and cos waves, but some additional
+   calculation will be required at each step after the first
+   quarter wave.                                              */
+
+Adafruit_MCP4725 dac;
+
+// Set this value to 9, 8, 7, 6 or 5 to adjust the resolution
+#define DAC_RESOLUTION    (9)
+
+const PROGMEM uint16_t DACLookup_FullSine_9Bit[512] =
+{
+  2048, 2073, 2098, 2123, 2148, 2174, 2199, 2224,
+  2249, 2274, 2299, 2324, 2349, 2373, 2398, 2423,
+  2448, 2472, 2497, 2521, 2546, 2570, 2594, 2618,
+  2643, 2667, 2690, 2714, 2738, 2762, 2785, 2808,
+  2832, 2855, 2878, 2901, 2924, 2946, 2969, 2991,
+  3013, 3036, 3057, 3079, 3101, 3122, 3144, 3165,
+  3186, 3207, 3227, 3248, 3268, 3288, 3308, 3328,
+  3347, 3367, 3386, 3405, 3423, 3442, 3460, 3478,
+  3496, 3514, 3531, 3548, 3565, 3582, 3599, 3615,
+  3631, 3647, 3663, 3678, 3693, 3708, 3722, 3737,
+  3751, 3765, 3778, 3792, 3805, 3817, 3830, 3842,
+  3854, 3866, 3877, 3888, 3899, 3910, 3920, 3930,
+  3940, 3950, 3959, 3968, 3976, 3985, 3993, 4000,
+  4008, 4015, 4022, 4028, 4035, 4041, 4046, 4052,
+  4057, 4061, 4066, 4070, 4074, 4077, 4081, 4084,
+  4086, 4088, 4090, 4092, 4094, 4095, 4095, 4095,
+  4095, 4095, 4095, 4095, 4094, 4092, 4090, 4088,
+  4086, 4084, 4081, 4077, 4074, 4070, 4066, 4061,
+  4057, 4052, 4046, 4041, 4035, 4028, 4022, 4015,
+  4008, 4000, 3993, 3985, 3976, 3968, 3959, 3950,
+  3940, 3930, 3920, 3910, 3899, 3888, 3877, 3866,
+  3854, 3842, 3830, 3817, 3805, 3792, 3778, 3765,
+  3751, 3737, 3722, 3708, 3693, 3678, 3663, 3647,
+  3631, 3615, 3599, 3582, 3565, 3548, 3531, 3514,
+  3496, 3478, 3460, 3442, 3423, 3405, 3386, 3367,
+  3347, 3328, 3308, 3288, 3268, 3248, 3227, 3207,
+  3186, 3165, 3144, 3122, 3101, 3079, 3057, 3036,
+  3013, 2991, 2969, 2946, 2924, 2901, 2878, 2855,
+  2832, 2808, 2785, 2762, 2738, 2714, 2690, 2667,
+  2643, 2618, 2594, 2570, 2546, 2521, 2497, 2472,
+  2448, 2423, 2398, 2373, 2349, 2324, 2299, 2274,
+  2249, 2224, 2199, 2174, 2148, 2123, 2098, 2073,
+  2048, 2023, 1998, 1973, 1948, 1922, 1897, 1872,
+  1847, 1822, 1797, 1772, 1747, 1723, 1698, 1673,
+  1648, 1624, 1599, 1575, 1550, 1526, 1502, 1478,
+  1453, 1429, 1406, 1382, 1358, 1334, 1311, 1288,
+  1264, 1241, 1218, 1195, 1172, 1150, 1127, 1105,
+  1083, 1060, 1039, 1017,  995,  974,  952,  931,
+   910,  889,  869,  848,  828,  808,  788,  768,
+   749,  729,  710,  691,  673,  654,  636,  618,
+   600,  582,  565,  548,  531,  514,  497,  481,
+   465,  449,  433,  418,  403,  388,  374,  359,
+   345,  331,  318,  304,  291,  279,  266,  254,
+   242,  230,  219,  208,  197,  186,  176,  166,
+   156,  146,  137,  128,  120,  111,  103,   96,
+    88,   81,   74,   68,   61,   55,   50,   44,
+    39,   35,   30,   26,   22,   19,   15,   12,
+    10,    8,    6,    4,    2,    1,    1,    0,
+     0,    0,    1,    1,    2,    4,    6,    8,
+    10,   12,   15,   19,   22,   26,   30,   35,
+    39,   44,   50,   55,   61,   68,   74,   81,
+    88,   96,  103,  111,  120,  128,  137,  146,
+   156,  166,  176,  186,  197,  208,  219,  230,
+   242,  254,  266,  279,  291,  304,  318,  331,
+   345,  359,  374,  388,  403,  418,  433,  449,
+   465,  481,  497,  514,  531,  548,  565,  582,
+   600,  618,  636,  654,  673,  691,  710,  729,
+   749,  768,  788,  808,  828,  848,  869,  889,
+   910,  931,  952,  974,  995, 1017, 1039, 1060,
+  1083, 1105, 1127, 1150, 1172, 1195, 1218, 1241,
+  1264, 1288, 1311, 1334, 1358, 1382, 1406, 1429,
+  1453, 1478, 1502, 1526, 1550, 1575, 1599, 1624,
+  1648, 1673, 1698, 1723, 1747, 1772, 1797, 1822,
+  1847, 1872, 1897, 1922, 1948, 1973, 1998, 2023
+};
+
+const PROGMEM uint16_t DACLookup_FullSine_8Bit[256] =
+{
+  2048, 2098, 2148, 2198, 2248, 2298, 2348, 2398,
+  2447, 2496, 2545, 2594, 2642, 2690, 2737, 2784,
+  2831, 2877, 2923, 2968, 3013, 3057, 3100, 3143,
+  3185, 3226, 3267, 3307, 3346, 3385, 3423, 3459,
+  3495, 3530, 3565, 3598, 3630, 3662, 3692, 3722,
+  3750, 3777, 3804, 3829, 3853, 3876, 3898, 3919,
+  3939, 3958, 3975, 3992, 4007, 4021, 4034, 4045,
+  4056, 4065, 4073, 4080, 4085, 4089, 4093, 4094,
+  4095, 4094, 4093, 4089, 4085, 4080, 4073, 4065,
+  4056, 4045, 4034, 4021, 4007, 3992, 3975, 3958,
+  3939, 3919, 3898, 3876, 3853, 3829, 3804, 3777,
+  3750, 3722, 3692, 3662, 3630, 3598, 3565, 3530,
+  3495, 3459, 3423, 3385, 3346, 3307, 3267, 3226,
+  3185, 3143, 3100, 3057, 3013, 2968, 2923, 2877,
+  2831, 2784, 2737, 2690, 2642, 2594, 2545, 2496,
+  2447, 2398, 2348, 2298, 2248, 2198, 2148, 2098,
+  2048, 1997, 1947, 1897, 1847, 1797, 1747, 1697,
+  1648, 1599, 1550, 1501, 1453, 1405, 1358, 1311,
+  1264, 1218, 1172, 1127, 1082, 1038,  995,  952,
+   910,  869,  828,  788,  749,  710,  672,  636,
+   600,  565,  530,  497,  465,  433,  403,  373,
+   345,  318,  291,  266,  242,  219,  197,  176,
+   156,  137,  120,  103,   88,   74,   61,   50,
+    39,   30,   22,   15,   10,    6,    2,    1,
+     0,    1,    2,    6,   10,   15,   22,   30,
+    39,   50,   61,   74,   88,  103,  120,  137,
+   156,  176,  197,  219,  242,  266,  291,  318,
+   345,  373,  403,  433,  465,  497,  530,  565,
+   600,  636,  672,  710,  749,  788,  828,  869,
+   910,  952,  995, 1038, 1082, 1127, 1172, 1218,
+  1264, 1311, 1358, 1405, 1453, 1501, 1550, 1599,
+  1648, 1697, 1747, 1797, 1847, 1897, 1947, 1997
+};
+
+const PROGMEM uint16_t DACLookup_FullSine_7Bit[128] =
+{
+  2048, 2148, 2248, 2348, 2447, 2545, 2642, 2737,
+  2831, 2923, 3013, 3100, 3185, 3267, 3346, 3423,
+  3495, 3565, 3630, 3692, 3750, 3804, 3853, 3898,
+  3939, 3975, 4007, 4034, 4056, 4073, 4085, 4093,
+  4095, 4093, 4085, 4073, 4056, 4034, 4007, 3975,
+  3939, 3898, 3853, 3804, 3750, 3692, 3630, 3565,
+  3495, 3423, 3346, 3267, 3185, 3100, 3013, 2923,
+  2831, 2737, 2642, 2545, 2447, 2348, 2248, 2148,
+  2048, 1947, 1847, 1747, 1648, 1550, 1453, 1358,
+  1264, 1172, 1082,  995,  910,  828,  749,  672,
+   600,  530,  465,  403,  345,  291,  242,  197,
+   156,  120,   88,   61,   39,   22,   10,    2,
+     0,    2,   10,   22,   39,   61,   88,  120,
+   156,  197,  242,  291,  345,  403,  465,  530,
+   600,  672,  749,  828,  910,  995, 1082, 1172,
+  1264, 1358, 1453, 1550, 1648, 1747, 1847, 1947
+};
+
+const PROGMEM uint16_t DACLookup_FullSine_6Bit[64] =
+{
+  2048, 2248, 2447, 2642, 2831, 3013, 3185, 3346,
+  3495, 3630, 3750, 3853, 3939, 4007, 4056, 4085,
+  4095, 4085, 4056, 4007, 3939, 3853, 3750, 3630,
+  3495, 3346, 3185, 3013, 2831, 2642, 2447, 2248,
+  2048, 1847, 1648, 1453, 1264, 1082,  910,  749,
+   600,  465,  345,  242,  156,   88,   39,   10,
+     0,   10,   39,   88,  156,  242,  345,  465,
+   600,  749,  910, 1082, 1264, 1453, 1648, 1847
+};
+
+const PROGMEM uint16_t DACLookup_FullSine_5Bit[32] =
+{
+  2048, 2447, 2831, 3185, 3495, 3750, 3939, 4056,
+  4095, 4056, 3939, 3750, 3495, 3185, 2831, 2447,
+  2048, 1648, 1264,  910,  600,  345,  156,   39,
+     0,   39,  156,  345,  600,  910, 1264, 1648
+};
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+static const uint32_t BUFFER_SIZE = 32;
+uint8_t* spi_master_tx_buf;
+uint8_t* spi_master_rx_buf;
+
+void set_buffer() {
+    for (uint32_t i = 0; i < BUFFER_SIZE; i++) {
+        spi_master_tx_buf[i] = i & 0xFF;
+    }
+    memset(spi_master_rx_buf, 0, BUFFER_SIZE);
+}
+
+
+#define VSPI_MISO   MISO
+#define VSPI_MOSI   MOSI
+#define VSPI_SCLK   SCK
+#define VSPI_SS     SS 
+
+static const int spiClk = 250000; // 250 kHz?
+
+SPIClass * vspi = NULL;
+uint8_t slaveRecv = 0;
+
+void spiCommand(SPIClass *spi, byte data) {}
+
+void setup(void)
+{
+  // to use DMA buffer, use these methods to allocate buffer
+  spi_master_tx_buf = master.allocDMABuffer(BUFFER_SIZE);
+  spi_master_rx_buf = master.allocDMABuffer(BUFFER_SIZE);
+
+  set_buffer();
+
+  master.setDataMode(SPI_MODE0);           // default: SPI_MODE0
+  master.setFrequency(100000);            // default: 8MHz (too fast for bread board...)
+  master.setMaxTransferSize(BUFFER_SIZE);  // default: 4092 bytes
+
+  // begin() after setting
+  master.begin();  // default: HSPI (CS: 15, CLK: 14, MOSI: 13, MISO: 12)
+  
+  Serial.begin(115200);
+  Wire.begin();
+
+  //vspi = new SPIClass(VSPI);
+  
+  //vspi->begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS); //SCLK, MISO, MOSI, SS
+  
+  Serial.println("Booting");
+  
+  //set up slave select pins as outputs as the Arduino API
+  //doesn't handle automatically pulling SS low
+  pinMode(vspi->pinSS(), OUTPUT); //VSPI SS
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1);
+  }  
+ // For Adafruit MCP4725A1 the address is 0x62 (default) or 0x63 (ADDR pin tied to VCC)
+  // For MCP4725A0 the address is 0x60 or 0x61
+  // For MCP4725A2 the address is 0x64 or 0x65
+  dac.begin(0x63);
+    
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    // for(;;); // Don't proceed, loop forever
+  }
+  
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+  
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+  display.display(); // Update screen 
+
+  // Reset devices
+  MCP342x::generalCallReset();
+  delay(1); // MC342x needs 300us to settle, wait 1ms
+  
+  // Check device present
+  Wire.requestFrom(address, (uint8_t)1);
+  if (!Wire.available()) {
+    Serial.print("No device found at address ");
+    Serial.println(address, HEX);
+    while (1)
+      ;
+  }
+
+}
+
+void loop(void)
+{
+  uint16_t i;
+
+  for (i = 0; i < 512; i++) {
+    uint8_t ptr=0;
+    
+    dac.setVoltage(pgm_read_word(&(DACLookup_FullSine_9Bit[i])), false);
+
+    adc0 = ads.readADC_SingleEnded(0);
+    spi_master_tx_buf[ptr]  = highByte(adc0);
+    spi_master_tx_buf[ptr++] = lowByte(adc0);
+    adc1 = ads.readADC_SingleEnded(1);
+    spi_master_tx_buf[ptr++] = highByte(adc1);
+    spi_master_tx_buf[ptr++] = lowByte(adc1);
+    adc2 = ads.readADC_SingleEnded(2);
+    spi_master_tx_buf[ptr++] = highByte(adc2);
+    spi_master_tx_buf[ptr++] = lowByte(adc2);
+    adc3 = ads.readADC_SingleEnded(3);
+    spi_master_tx_buf[ptr++] = highByte(adc3);
+    spi_master_tx_buf[ptr++] = lowByte(adc3);
+    
+    volts0 = ads.computeVolts(adc0);
+    volts1 = ads.computeVolts(adc1);
+    volts2 = ads.computeVolts(adc2);
+    volts3 = ads.computeVolts(adc3);
+  
+    Serial.println("-----------------------------------------------------------");
+    Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0); Serial.println("V");
+    Serial.print("AIN1: "); Serial.print(adc1); Serial.print("  "); Serial.print(volts1); Serial.println("V");
+    Serial.print("AIN2: "); Serial.print(adc2); Serial.print("  "); Serial.print(volts2); Serial.println("V");
+    Serial.print("AIN3: "); Serial.print(adc3); Serial.print("  "); Serial.print(volts3); Serial.println("V");
+            
+
+    MCP342x::Config status;
+  
+    // Initiate a conversion; convertAndRead() will wait until it can be read
+    uint8_t err1 = adc.convertAndRead(MCP342x::channel1, MCP342x::oneShot,
+  				   MCP342x::resolution12, MCP342x::gain1,
+  				   1000000, ch1, status);
+    uint8_t err2 = adc.convertAndRead(MCP342x::channel2, MCP342x::oneShot,
+             MCP342x::resolution12, MCP342x::gain1,
+             1000000, ch2, status);
+    
+    if (err1) {
+      Serial.print("CH1 Err");
+      //Serial.println(err1);
+  
+      display.clearDisplay();
+      display.setCursor(0, 0);     // Start at top-left corner    
+      display.print("CH1 Err");
+      //display.println(err1);
+    }
+    else {
+      ampsch1=(float)(ch1-acsOffset) / acsmvA;
+      spi_master_tx_buf[ptr++] = highByte(ch1-acsOffset);
+      spi_master_tx_buf[ptr++] = lowByte(ch1-acsOffset);      
+      
+      sprintf(buff, "CH1: (%d) %.2fa ", ch1, ampsch1);
+      Serial.print(buff);
+      //Serial.print("CH1: ");
+      //Serial.println(ch1);
+  
+      display.clearDisplay();
+      display.setCursor(0, 0);     // Start at top-left corner    
+      display.println(buff);
+      //display.println(ch1);
+    }
+  
+    if (err2) {
+      Serial.println("CH2 Error");
+      //Serial.println(err2);
+  
+      //display.clearDisplay();
+      //display.setCursor(0, 0);     // Start at top-left corner    
+      display.println("CH2 Error: ");
+      //display.println(err2);
+    }
+    else {
+      ampsch2=(float)(ch2-acsOffset) / acsmvA;
+      spi_master_tx_buf[ptr++] = highByte(ch2-acsOffset);
+      spi_master_tx_buf[ptr++] = lowByte(ch2-acsOffset);      
+      
+      sprintf(buff, "CH2: (%d) %.2fa", ch2, ampsch2);
+      Serial.println(buff);
+      //Serial.println(ch2);
+  
+      //display.clearDisplay();
+      //display.setCursor(0, 0);     // Start at top-left corner    
+      display.println(buff);
+      //display.println(ch2);
+    }
+  
+    //use the SPI buses
+    //spiCommand(vspi, adc0); // junk data to illustrate usage    
+    
+    display.display(); // update display
+    
+    // start and wait to complete transaction
+    master.transfer(spi_master_tx_buf, spi_master_rx_buf, BUFFER_SIZE);
+
+    // show received data (if needed)
+    for (size_t i = 0; i < BUFFER_SIZE; ++i) {
+        Serial.printf("%d ", spi_master_rx_buf[i]);
+    }
+    Serial.printf("\n");
+    
+    for (uint8_t x=0; x<200; x++){
+      ArduinoOTA.handle();
+      delay(1);
+    }
+  }
+}
+
+void spiCommand(SPIClass *spi, byte data) {
+  byte recv = 0;
+  //use it as you would the regular arduino SPI API
+  spi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  digitalWrite(spi->pinSS(), LOW); //pull SS slow to prep other end for transfer
+  delayMicroseconds(50); /// wait an entire ms for the slave
+  
+  spi->transfer(data); // send data
+
+  recv << spi->transfer(0xff); // send dummy byte
+  
+  delayMicroseconds(50); /// wait an entire ms for the slave
+  digitalWrite(spi->pinSS(), HIGH); //pull ss high to signify end of data transfer
+
+  spi->endTransaction();
+
+  sprintf(buff, "Sent: %u Recv: %u", data, recv);
+  Serial.print(buff);
+
+  display.setCursor(0, 16);     // Start at top-left corner    
+  display.println(buff);
+}
+
