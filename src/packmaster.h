a33b237815975257d69bea75af6cff6bfffa234b
gordonthree <gordonthree@gmail.com>
2022-08-02 18:51:49 -0400

    trying some modular code

---
+++ src/packmaster.h
@@ -0,0 +1,21 @@
+#ifndef packmaster_h
+#define packmaster_h
+#include "Arduino.h" 
+#endif
+
+class packmaster
+{
+private:
+    /* data */
+public:
+    packmaster(/* args */);
+    ~packmaster();
+};
+
+packmaster::packmaster(/* args */)
+{
+}
+
+packmaster::~packmaster()
+{
+}
